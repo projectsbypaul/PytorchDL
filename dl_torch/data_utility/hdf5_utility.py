fixed conversion error in weihts calculation of fcb and mfcb

dl_torch/data_utility/hdf5_utility.py
@@ -3,6 +3,8 @@ import argparse
 import pickle
 from typing import Optional
 import torch
+from numpy.f2py.auxfuncs import throw_error
+
 from dl_torch.data_utility.HDF5Dataset import HDF5Dataset
 from visualization import color_templates
 import numpy as np
@@ -185,7 +187,7 @@ def __get_indicis_by_class(result_loc, ignore_index):
 
     return unique_classes, index_container
 
-def get_class_distribution(result_loc,
+def get_class_distribution(result_loc, class_template,
                      ignore_names=("Inside", "Outside"),
                      use_tiebreak_noise=False,
                      mark_empty_as_unknown=True):
@@ -209,7 +211,14 @@ def get_class_distribution(result_loc,
         count_collection = np.asarray(count_collection)
 
     # ---- classes & mappings ----
-    class_temp = color_templates.inside_outside_color_template_abc()
+
+    if class_template == "inside_outside":
+        class_temp = color_templates.inside_outside_color_template_abc()
+    elif class_template == "edge":
+        class_temp = color_templates.edge_color_template_abc()
+    else:
+        raise ValueError(f"Template '{class_template}' not implemented.")
+
     class_list = color_templates.get_class_list(class_temp)
     class_to_index = color_templates.get_class_to_index_dict(class_temp)
     index_to_class = color_templates.get_index_to_class_dict(class_temp)

dl_torch/model_utility/Classifcation_Helpers.py
@@ -12,22 +12,21 @@ def get_voxel_count_from_stats_binary(stat_bin_path: str):
 
 def get_fcb_weights_from_voxel_count(voxel_count: np.array):
 
-    class_count = np.sum(voxel_count, axis=0)
-    total_count = np.sum(class_count, axis=0)
+    total_count = np.sum(voxel_count, axis=0)
 
     #weights for pure frequency balancing
-    f_c = class_count / total_count
+    f_c =  voxel_count / total_count
     w_fcb = 1 / f_c
 
     return w_fcb
 
 def get_fcb_median_weights_from_class_count(voxel_count: np.array):
 
-    class_count = np.sum(voxel_count, axis=0)
-    total_count = np.sum(class_count, axis=0)
+
+    total_count = np.sum(voxel_count, axis=0)
 
     # weights for median frequency balancing
-    f_c = class_count / total_count
+    f_c = voxel_count / total_count
     f_median = np.median(f_c)
     w_median_fcb = f_median / f_c
 